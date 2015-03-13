80f37c88e9877a5213b36c3b60fa93cd18d9d57b
Patrick LORIN <lorinpatrick@hotmail.fr>
2015-03-13 16:00:45 +0100

    sdqdqsd
    dfdsfs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 import static spark.Spark.*;
+import spark.*;
 /**
  *
  * @author Patrick
@@ -12,6 +13,14 @@ public class Main
 {
     public static void main(String[] args)
     {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
+        }
+        setPort(port);
         get("/", (req, res) -> {
             String html = "<!DOCTYPE html><head></head><body>";
             FizzBuzz fb = new FizzBuzz();
