b663895377d1b1e3a617d9b81cb944df9bcb4dcd
Patrick LORIN <lorinpatrick@hotmail.fr>
2015-03-13 17:06:51 +0100

    last
    last

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import static spark.Spark.*;
 import spark.*;
+import fr.patrick.lorin.fizzbuzz.FizzBuzz;
 /**
  *
  * @author Patrick
  */
-public class Main
+public class App
 {
     public static void main(String[] args)
     {
         setPort(Integer.parseInt(System.getenv("PORT")));
-        get("/", (req, res) -> {
+        get("/fizzbuzz", (req, res) -> {
             String html = "<!DOCTYPE html><head></head><body>";
             FizzBuzz fb = new FizzBuzz();
             for(int i = 1; i<101;i++)
